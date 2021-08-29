7448e5aa09cc267d4cf2bba8b21badd0f6091a01
Shubham-knoldus <shubham.shrivastava@knoldus.com>
2021-08-29 23:46:00 +0530

    Maven assignment

---
+++ cal-impl/src/main/java/Application.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public final class Application {
+    private Application() {
+    }
+
+    /**
+     * @param args
+     */
+
+    @edu.umd.cs.findbugs.annotations.SuppressFBWarnings({"DLS_DEAD_LOCAL_STORE", "DM_DEFAULT_ENCODING"})
+    public static void main(final String[] args) {
+
+        System.out.println("This is a basic calculator");
+        Calculator c = new Calculator();
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the two numbers: ");
+        int num1 = s.nextInt();
+        int num2 = s.nextInt();
+
+        Calculator calci = new Calculator();
+        calci.multiplication(num1, num2);
+        calci.division(num1, num2);
+        calci.addition(num1, num2);
+        calci.substraction(num1, num2);
+
+    }
+}
